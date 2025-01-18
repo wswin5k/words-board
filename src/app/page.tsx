b93c840c562e63d15e291d8c7312c04cd1721911
wswin <wswin@protonmail.com>
2025-01-18 17:53:13 +0100

    Update words list and add pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,40 @@
+"use client";
 import * as React from "react";
 import wordsData from "./wordsData";
+import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-function WordEntry({ word, key }) {
+const innerTheme = createTheme({
+  palette: {
+    text: {
+      primary: {
+        main: "#ffffff",
+      },
+      secondary: {
+        main: "#ffffff",
+      },
+    },
+  },
+});
+
+function WordEntry({ word, tdKey }) {
   //const url = `https://www.merriam-webster.com/dictionary/${word}`;
   const url = `https://onelook.com/?w=${word}`;
 
   return (
-    <td key={key} style={{ padding: "10px" }}>
+    <td key={tdKey} style={{ padding: "10px" }}>
       <a href={url}>{word}</a>
     </td>
   );
 }
 
-function RowEntry({ words, key }) {
+function RowEntry({ words, trKey }) {
   return (
-    <tr key={key}>
+    <tr key={trKey}>
       {words.map((word, idx) => {
-        return <WordEntry word={word} key={idx} />;
+        return <WordEntry word={word} key={idx} tdKey={idx} />;
       })}
     </tr>
   );
@@ -26,23 +44,41 @@ export default function Home() {
   const columnCount = 5;
   const rowCount = 20;
 
+  const [page, setPage] = React.useState(1);
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
   const words = wordsData.map((wordObj) => wordObj.word);
 
   const rowEntries = [];
-  for (let row = 0; row < rowCount; row++) {
+  const rowStart = (page - 1) * rowCount;
+  for (let row = rowStart; row < rowStart + rowCount; row++) {
     rowEntries.push(
       <RowEntry
         words={words.slice(row * columnCount, (row + 1) * columnCount)}
+        trKey={row}
         key={row}
       />,
     );
   }
 
   return (
-    <div>
-      <table>
-        <tbody>{rowEntries}</tbody>
-      </table>
-    </div>
+    <Stack spacing={2}>
+      <div>
+        <table>
+          <tbody>{rowEntries}</tbody>
+        </table>
+      </div>
+
+      <ThemeProvider theme={innerTheme}>
+        <Pagination
+          count={10}
+          page={page}
+          color="primary"
+          onChange={handleChange}
+        />
+      </ThemeProvider>
+    </Stack>
   );
 }
